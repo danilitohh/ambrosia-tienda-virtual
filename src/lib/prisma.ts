a56e411e65c81b935f55aec2de3f3091bbf5d644
danilo herrera hernandez <111337799+danilitohh@users.noreply.github.com>
2025-08-20 16:29:58 -0500

    fix: instancia de PrismaClient sin parámetros para compatibilidad con PrismaAdapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
